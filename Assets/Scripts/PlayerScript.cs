2be769ad767a1f6253ff60b035f238dd085d5949
JOMMUM <thdwhddnd427@naver.com>
2022-07-16 23:29:40 +0900

    플레이어 벽타기 구현
    -벽에 붙을 시 상하좌우로 벽 이동 가능
    -벽 끝에 다다르거나 점프를 누를 시 튕겨나감
    -플레이어 벽타기 애니메이션 적용

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -15,9 +15,11 @@ public class PlayerScript : MonoBehaviour
     bool useDoubleJump;
 
     bool isJump;
+    bool isClimb;
 
     public int moveSpd = 5; //이동속도
     public int runSpd = 10; //달리기 속도
+    public int climbSpd = 3; //기어가는 속도
     public int jumpPower; //점프력
 
 
@@ -34,13 +36,18 @@ public class PlayerScript : MonoBehaviour
 
         if (canMove)
         {
-            PlayerMove();
-            PlayerJump();
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            PlayerMove(); //이동
+            PlayerJump(); //점프
+            PlayerClimb(); //벽 타기
         }
 
         animator.SetBool("isWalk", moveDir != Vector3.zero);
         animator.SetBool("isRun", moveDir != Vector3.zero && Input.GetAxis("Run") != 0);
         animator.SetBool("isJump", isJump);
+        animator.SetBool("isClimb", isClimb);
+        animator.SetBool("isMoveClimb", moveDir != Vector3.zero && isClimb);
     }
 
     void OnCollisionEnter(Collision collision)
@@ -54,22 +61,23 @@ public class PlayerScript : MonoBehaviour
 
     void PlayerMove()
     {
-        moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-        //이동 중일 시
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        if (!isClimb)
         {
-            //이동 방향으로 플레이어 회전
-            Vector3 camForward = new Vector3(camera.transform.forward.x, 0, camera.transform.forward.z).normalized;
-            Vector3 camRight = new Vector3(camera.transform.right.x, 0, camera.transform.right.z).normalized;
-            Vector3 playerDir = camForward * moveDir.z + camRight * moveDir.x; //플레이어의 방향은 카메라의 벡터값과 키보드 입력값을 통해 구함
-            transform.forward = playerDir;
+            //이동 중일 시
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                //이동 방향으로 플레이어 회전
+                Vector3 camForward = new Vector3(camera.transform.forward.x, 0, camera.transform.forward.z).normalized;
+                Vector3 camRight = new Vector3(camera.transform.right.x, 0, camera.transform.right.z).normalized;
+                Vector3 playerDir = camForward * moveDir.z + camRight * moveDir.x; //플레이어의 방향은 카메라의 벡터값과 키보드 입력값을 통해 구함
+                transform.forward = playerDir;
 
-            //목표 지점으로 이동
-            if(Input.GetAxis("Run") != 0) //달리기
-                transform.position += playerDir * runSpd * Time.deltaTime;
-            else //걷기
-                transform.position += playerDir * moveSpd * Time.deltaTime;
+                //목표 지점으로 이동
+                if (Input.GetAxis("Run") != 0) //달리기
+                    transform.position += playerDir * runSpd * Time.deltaTime;
+                else //걷기
+                    transform.position += playerDir * moveSpd * Time.deltaTime;
+            }
         }
     }
 
@@ -77,6 +85,18 @@ public class PlayerScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            //벽 오르는 중일 시
+            if (isClimb)
+            {
+                isClimb = false;
+                isJump = true;
+
+                //뒤로 튕겨나감
+                rigidbody.AddForce(-transform.forward * 3, ForceMode.Impulse);
+
+                return;
+            }
+
             //1단 점프
             if (!isJump)
             {
@@ -93,6 +113,53 @@ public class PlayerScript : MonoBehaviour
                 rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
                 return;
             }
+        }
+    }
+
+    void PlayerClimb()
+    {
+        Vector3 raycastPos = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z); //플레이어가 바라보는 방향 (y값이 발에 맞춰져 있으므로 값을 조금 더해줌)
+
+        //레이캐스트를 통해 벽 찾기
+        if (Physics.Raycast(raycastPos, transform.forward, out RaycastHit hit, 0.3f))
+        {
+            //벽이 있을 경우
+            if (hit.collider != null)
+            {
+                if (!isClimb)
+                {
+                    //점프 초기화
+                    isJump = false;
+                    useDoubleJump = false;
+
+                    rigidbody.velocity = Vector3.zero; //AddForce 값 초기화
+                    transform.rotation = Quaternion.LookRotation(-hit.normal); //벽의 반대 방향으로 캐릭터회전
+
+                    rigidbody.useGravity = false; //벽을 오르는 중에는 중력의 영향을 받지 않음
+                }
+
+                isClimb = true;
+
+                //벽 이동
+                Vector3 playerDir = transform.up * moveDir.z + transform.right * moveDir.x; //플레이어의 방향은 캐릭터의 벡터값과 키보드 입력값을 통해 구함
+                transform.position += playerDir * climbSpd * Time.deltaTime;
+            }
+        }
+        //벽이 없을 경우
+        else if(isClimb && hit.collider == null)
+        {
+            isClimb = false;
+
+            //뒤로 튕겨나감
+            rigidbody.AddForce(transform.up * 10, ForceMode.Impulse);
+            isJump = true;
+        }
+
+        //벽을 오르는 중이 아닐 시
+        if(!isClimb)
+        {
+            //중력 활성화
+            rigidbody.useGravity = true;
         }
     }
 }
