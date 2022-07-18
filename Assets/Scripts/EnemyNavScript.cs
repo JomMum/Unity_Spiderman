c4a76455790c20998c22190574905fea1154f225
zkfmxk <99245221+zkfmxk@users.noreply.github.com>
2022-07-18 09:35:21 +0900

    AI완성
    다했다 ㅋ
    '

--- Assets/Scripts/EnemyNavScript.cs
+++ Assets/Scripts/EnemyNavScript.cs
@@ -5,75 +5,50 @@ using UnityEngine.AI;
 
 public class EnemyNavScript : MonoBehaviour
 {
+    public bool isAttacted = false;
+    GameObject targets = null; 
+    
     NavMeshAgent nav = null;
-    [SerializeField] Transform[] Waypoints = null;
-    int count = 0;
-
-        
-    
-      public Transform targets = null; 
-
-
-    
+  
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
-        InvokeRepeating("MoveToNextWay", 0f, 2f);
     }
 
     void Update()
     {
+        //타겟 발견 시 타겟을 향해 이동
         if(targets != null)
         {
-            nav.SetDestination(targets.position);
-        }
-    }
-
-    void MoveToNextWay()
-    {
-        if(targets == null)
-        {
-            if(nav.velocity == Vector3.zero)
-            {
-                nav.SetDestination(Waypoints[count++].position);
-
-                if(count >= Waypoints.Length)
-                {
-                    count = 0;
-                }
-
-            }
+            Debug.Log(targets);
+            nav.SetDestination(targets.transform.position);
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        //플레이어가 시야 내에 들어올 시
+        if (other.CompareTag("Player"))
         {
-            nav.SetDestination(targets.position); 
+            //해당 플레이어 저장
+            targets = other.gameObject;
             print("발견");
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
+        //플레이어가 시야 밖에 나갈 시
+        if (other.CompareTag("Player") && targets != null)
         {
             Remove_Target();
         }
     }
 
-
-    public void SetTarget(Transform transform)
-    {
-        CancelInvoke();
-        transform = targets;
-    }
-
     public void Remove_Target()
     {
         targets = null;
-        InvokeRepeating("MoveToNextWay", 0f, 2f);
+
     }
 }
        
